091a2ca85866453c4843deecfa354918d84c55cd
Daniel Wang <danielwpz@gmail.com>
2019-01-16 16:08:13 -0800

    enable use for router

--- test/test.js
+++ test/test.js
@@ -1,19 +1,30 @@
 const CrepeCake = require('../index');
 const HttpResponse = CrepeCake.HttpResponse;
+const crepecakeCommon = require('crepecake-common');
 
 const app = new CrepeCake();
 const router = new CrepeCake.Router();
+const tokenRouter = new CrepeCake.Router();
+
+tokenRouter
+  .post('/', (ctx) => {
+    return HttpResponse.created();
+  })
+  .delete('/', (ctx) => {
+    return HttpResponse.badRequest('no token');
+  });
 
 router
+  .use(async function printHeaders(ctx, next) {
+    console.log(ctx.headers);
+    await next();
+  })
   .get('/', (ctx) => {
     return 'ok';
   })
-  .post('/token', (ctx) => {
-    throw HttpResponse.internalError('cannot do it now', {app});
-  })
-  .delete('/token', (ctx) => {
-    return HttpResponse.badRequest('no token');
-  });
+  .use('/token', tokenRouter);
+
+app.use(crepecakeCommon);
 
 app.use(async function requestLogger(ctx, next) {
   console.log(`${ctx.request.method} ${ctx.request.url}`);
