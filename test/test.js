dd2a62b79be12a3ffda8ca763a220643d0c5ed52
Daniel Wang <danielwpz@gmail.com>
2019-03-08 17:36:03 -0800

    add tests

--- test/test.js
+++ test/test.js
@@ -1,38 +1,88 @@
 const CrepeCake = require('../index');
 const HttpResponse = CrepeCake.HttpResponse;
 const crepecakeCommon = require('crepecake-common');
+const supertest = require('supertest');
 
 const app = new CrepeCake();
-const router = new CrepeCake.Router();
-const tokenRouter = new CrepeCake.Router();
 
-tokenRouter
-  .post('/', (ctx) => {
-    return HttpResponse.created();
-  })
-  .delete('/', (ctx) => {
-    return HttpResponse.badRequest('no token');
+describe('Crepecake Test', () => {
+  let request, server;
+
+  before(() => {
+    app.use(crepecakeCommon());
+
+    const rootRouter = new CrepeCake.Router();
+    const subRouter = new CrepeCake.Router();
+
+    subRouter.get('/', (ctx) => {
+      return 'ok';
+    });
+
+    subRouter.post('/accepted', async (ctx) => {
+      return HttpResponse.accepted();
+    });
+
+    subRouter.get('/redirect', async (ctx) => {
+      return HttpResponse.found('/target');
+    });
+
+    subRouter.get('/notfound', (ctx) => {
+      return HttpResponse.notFound();
+    });
+
+    subRouter.get('/toolarge', (ctx) => {
+      throw HttpResponse.payloadTooLarge();
+    });
+
+    subRouter.get('/internalerror', (ctx) => {
+      throw HttpResponse.internalError();
+    });
+
+    rootRouter.use('/sub', subRouter);
+
+    app.use(rootRouter);
+    server = app.listen();
+    request = supertest(server);
   });
 
-router
-  .use(async function printHeaders(ctx, next) {
-    console.log(ctx.headers);
-    await next();
-  })
-  .get('/', (ctx) => {
-    return 'ok';
-  })
-  .use('/token', tokenRouter);
+  after(() => {
+    server.close();
+  });
 
-app.use(crepecakeCommon);
+  it('GET /', async () => {
+    await request
+      .get('/sub/')
+      .expect(200, 'ok');
+  });
 
-app.use(async function requestLogger(ctx, next) {
-  console.log(`${ctx.request.method} ${ctx.request.url}`);
-  await next();
-});
+  it('POST /accepted', async () => {
+    await request
+      .post('/sub/accepted')
+      .expect(202);
+  });
 
-app.use(router);
+  it('GET /redirect', async () => {
+    await request
+      .get('/sub/redirect')
+      .expect(302);
+  });
 
-app.listen(3000, () => {
-  console.log('CrepeCake server running...');
+  it('GET /notfound', async () => {
+    await request
+      .get('/sub/notfound')
+      .expect(404);
+  });
+
+  it('GET /toolarge', async () => {
+    await request
+      .get('/sub/toolarge')
+      .expect(413);
+  });
+
+  it('GET /internalerror', async () => {
+    await request
+      .get('/sub/internalerror')
+      .expect(500);
+  });
+
 });
