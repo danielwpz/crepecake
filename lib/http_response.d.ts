bbf1050be56c15653d90bfaec435d11717009a76
Daniel Wang <danielwpz@gmail.com>
2021-01-28 17:11:32 +0800

    fix

--- lib/http_response.d.ts
+++ lib/http_response.d.ts
@@ -1,13 +1,13 @@
 export class HttpResponse {
-  constructor (code: number, message: string)
+  constructor (readonly code: number, readonly message: string)
 }
 
 export class RedirectResponse extends HttpResponse {
-  constructor (code: number, message: string, url: string)
+  constructor (readonly code: number, readonly message: string, readonly url: string)
 }
 
 export class ServerErrorResponse extends HttpResponse {
-  constructor (code: number, message: string, reason: any)
+  constructor (readonly code: number, readonly message: string, readonly reason: any)
 }
 
 export declare function ok (message: any): HttpResponse
@@ -18,5 +18,19 @@ export declare function noContent (message: any): HttpResponse
 export declare function movedPermanently (url: string): RedirectResponse
 export declare function found (url: string): RedirectResponse
 export declare function seeOther (url: string): RedirectResponse
-export declare function notModified (message: any): RedirectResponse
+export declare function notModified (message: any): HttpResponse
 export declare function temporaryRedirect (url: string): RedirectResponse
+
+export declare function badRequest (message: any): HttpResponse
+export declare function unauthorized (message: any): HttpResponse
+export declare function forbidden (message: any): HttpResponse
+export declare function notFound (message: any): HttpResponse
+export declare function notAcceptable (message: any): HttpResponse
+export declare function lengthRequired (message: any): HttpResponse
+export declare function payloadTooLarge (message: any): HttpResponse
+export declare function unsupportedMediaType (message: any): HttpResponse
+export declare function tooManyRequests (message: any): HttpResponse
+
+export declare function internalError (message: any, reason: any): ServerErrorResponse
+export declare function notImplemented (message: any, reason: any): ServerErrorResponse
+export declare function serviceUnavailable (message: any, reason: any): ServerErrorResponse
